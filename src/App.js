2af23330cfd88a8370c228a0cf7d6e5dda4c5f7a
Abhishek Parashar <52458867+abhishekParashar-sudo@users.noreply.github.com>
2022-09-16 19:38:45 +0530

    +changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+      progress:0
+  }
+  setProgress = (progress) =>{
+    this.setState({progress : progress})
+  }
+   
+  render() {
+    return (
+      <div> 
+        <BrowserRouter>
+      
+        <Navbar />
+        <LoadingBar
+        height={3}
+        color='red'
+        progress={this.state.progress}/>
+      
+          <Routes>
+
+      <Route path = "/" element = {<News setProgress = {this.setProgress}key="general" country="in" category="general"/>}/>
+      <Route exact path = "/Home" element = {<News setProgress = {this.setProgress}key="home" country="in" category="general"/>}/>
+      <Route exact path = "/Business" element = {<News setProgress = {this.setProgress}key="business" country="in" category="business"/>}/>
+      <Route exact path = "/Entertainment" element = {<News setProgress = {this.setProgress}key="entertainment" country="in" category="entertainment"/>}/>
+      <Route exact path = "/Health" element = {<News setProgress = {this.setProgress}key="health"country="in" category="health"/>}/>
+      <Route exact path = "/Science" element = {<News setProgress = {this.setProgress}key="science"country="in" category="science"/>}/>
+      <Route exact path = "/Sports" element = {<News setProgress = {this.setProgress}key="sports"country="in" category="sports"/>}/>
+      <Route exact path = "/Technology" element = {<News setProgress = {this.setProgress}key="technology"country="in" category="technology"/>}/>
+
+      </Routes>
+      </BrowserRouter>
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+
+export class NewsItem extends Component {
+  render() {
+    let {title, description, imageUrl, url, author, time, source} = this.props;
+    return (
+      <div>
+        <div className="row">
+          <div className="col md-3">
+          <div className="card" style={{width : "18rem"}}>
+      <img src={imageUrl} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{title}...<span className="position-absolute top-0 translate-middle badge rounded-pill bg-warning" style={{left:"90%", zIndex:1}}>{source}</span></h5>
+        <p className="card-text">{description}...</p>
+        <small className="text-muted m-2 p-2">By {author} on {new Date(time).toGMTString()}</small>
+        <a href={url} target="_blank" rel="noreferrer" className="btn btn-primary d-block" style={{backgroundColor : "black"}}>Read More</a>
+      </div>
+    </div>
+          </div>
+
+                 </div>
+        </div>
+    )
+  }
+}
+
+export default NewsItem
